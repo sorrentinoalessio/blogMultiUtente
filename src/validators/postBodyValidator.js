88924ae6d4eb53fd0d2450d1b18675ce5f18a5ad
Alessio Sorrentino <sorrentinoalessio85@gmail.com>
2026-01-25 22:23:58 +0100

    update body validator

--- src/validators/postBodyValidator.js
+++ src/validators/postBodyValidator.js
@@ -8,7 +8,7 @@ const bodyValidator = Joi.object({
     title: Joi.string().required().min(3).max(256), // titolo obligatorio
     description: Joi.string().required().min(3),// descrizione obligatorio
     status: Joi.string().valid('public', 'draft').default('draft'),// status di default mette draft, se messo prende solo public o draft
-    datePost: Joi.date().default(() => new Date(), 'current date'), // mette data creazione post se non inviato
+    datePost: Joi.date(), // mette data creazione post se non inviato
     tag: Joi.array().items(Joi.string().min(3).max(24)), // tag come array di stringhe
     imagePost: Joi.string()
 
