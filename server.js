bf3f6e9a5036a314ce94453c72f7bbcfafa0b05f
Alessio Sorrentino <sorrentinoalessio85@gmail.com>
2026-02-01 19:51:44 +0100

    inserito middleware imageCreationMiddleware e post e get

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ import { connect } from './database.js';
 import {Server} from 'socket.io';
 import http from 'http';
 import SocketIoInitializer from './src/SocketIoInitializer.js';
-//app inserimento immagine
-//  ---------
-import multer from "multer"; 
-import path from "path";
-import { fileURLToPath } from "url";
-//------------
-
 export const host = 'localhost';
 export const port = 3001;
 const app = express();
@@ -20,36 +13,8 @@ app.use(express.json());
 
 //app inserimento immagine
 // -----------
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-// Set up Multer storage
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
-const upload = multer({ storage: storage });
-
-app.get("/", (req, res) => {
-  res.send(`
-    <h1>File Upload Demo</h1>
-    <form action="/upload" method="post" enctype="multipart/form-data">
-      <input type="file" name="uploadedFile" />
-      <button type="submit">Upload</button>
-    </form>
-  `);
-});
-
-app.post("/upload", upload.single('uploadedFile'), (req, res) => {
-  console.log(req.file); // Contains file info
-  res.send(`File uploaded successfully: ${req.file.filename}`);
-});
 
 //---------------
-
 
 await connect()
 const httpServer = http.createServer(app);
--- src/routes/PostRoutes.js
+++ src/routes/PostRoutes.js
@@ -5,6 +5,8 @@ import { add } from '../controllers/postControllers/addPostController.js';
 import { getTag } from '../controllers/postControllers/tagPostController.js';
 import { getPosts, getPostByIdPostAndUserId, getListPublicPost, updatePostStatus } from '../controllers/postControllers/addPostController.js';
 import { postUpdateBodyValidator } from '../validators/postUpdateBodyValidator.js';
+import imageCreationMiddleware from '../middlewares/imageCreationMiddleware.js';
+
 
 
 export class PostRoutes {
@@ -15,8 +17,26 @@ export class PostRoutes {
     router.get('/user/post/:id', checkAuthorizationMiddleware, postIdParamValidator, getPostByIdPostAndUserId);
     router.get('/post/', getListPublicPost);
     router.patch('/user/post/status/:id', checkAuthorizationMiddleware, postUpdateBodyValidator, updatePostStatus);
-
+    router.post("/upload", checkAuthorizationMiddleware, imageCreationMiddleware);
+    router.get("/form/upload", (req, res) => {
+      res.send(`
+    <h1>File Upload Demo</h1>
+    <form action="/upload" method="post" enctype="multipart/form-data">
+      <input type="file" name="uploadedFile" />
+      <button type="submit">Upload</button>
+    </form>
+  `);
+    });
   }
 }
 
-
+//checkAuthorizationMiddleware,
+/*app.get("/", (req, res) => {
+  res.send(`
+    <h1>File Upload Demo</h1>
+    <form action="/upload" method="post" enctype="multipart/form-data">
+      <input type="file" name="uploadedFile" />
+      <button type="submit">Upload</button>
+    </form>
+  `);
+});*/
