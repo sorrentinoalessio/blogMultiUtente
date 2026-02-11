b7fdcf738947d5eb5cd4caa35626694550cc566e
Alessio Sorrentino <sorrentinoalessio85@gmail.com>
2026-02-12 00:43:46 +0100

    update swagger url  https://alessio-be.longwavestudio.dev/

--- swagger.js
+++ swagger.js
@@ -12,7 +12,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: process.env.SWAGGER_URL || "http://localhost:3001" ,
+        url: "https://alessio-be.longwavestudio.dev/" ,
       },
     ],
     components: {
