fda9d4adfb7d766793fba3048087c38ea869b568
sorrentinoalessio <sorrentinoalessio85@gmail.com>
2026-08-05 22:58:42 +0200

    fix: aggiungi middleware upload immagine su route update post

--- src/routes/PostRoutes.js
+++ src/routes/PostRoutes.js
@@ -3,8 +3,8 @@ import { postIdParamValidator } from '../validators/postIdParamsValidator.js';
 import checkAuthorizationMiddleware from '../middlewares/checkAuthorizationMiddleware.js';
 import postImageCreationMiddleware from '../middlewares/postImageCreationMiddleware.js';
 import { createPost } from '../controllers/postControllers/addPostController.js';
-import { getTag, deleteTag} from '../controllers/postControllers/tagPostController.js';
-import { getPosts, getPostByIdPostAndUserId, getListPublicPosts, updatePost , getListPublicPost} from '../controllers/postControllers/addPostController.js';
+import { getTag, deleteTag } from '../controllers/postControllers/tagPostController.js';
+import { getPosts, getPostByIdPostAndUserId, getListPublicPosts, updatePost, getListPublicPost } from '../controllers/postControllers/addPostController.js';
 import { postUpdateBodyValidator } from '../validators/postUpdateBodyValidator.js';
 
 
@@ -16,8 +16,7 @@ export class PostRoutes {
     router.patch('/user/tag/delete/:id', checkAuthorizationMiddleware, postIdParamValidator, deleteTag); // elimina tag da id dell post user
     router.get('/user/post/:id', checkAuthorizationMiddleware, postIdParamValidator, getPostByIdPostAndUserId); // list post by user
     router.get('/post/', getListPublicPosts); // list post public
-    router.get('/post/:id',postIdParamValidator, getListPublicPost); // list post public
-    router.patch('/user/post/update/:id', checkAuthorizationMiddleware, postUpdateBodyValidator, updatePost); // update post da id post
-       
+    router.get('/post/:id', postIdParamValidator, getListPublicPost); // list post public
+    router.patch('/user/post/update/:id', checkAuthorizationMiddleware, postImageCreationMiddleware, postUpdateBodyValidator, updatePost);
   }
 }
