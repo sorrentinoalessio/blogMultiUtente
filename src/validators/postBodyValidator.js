362dcfa9a2fd98f391adcfd0c84467beae94277d
sorrentinoalessio <sorrentinoalessio85@gmail.com>
2026-09-21 00:21:12 +0200

    modificato eventDate

--- src/validators/postBodyValidator.js
+++ src/validators/postBodyValidator.js
@@ -8,10 +8,11 @@ const bodyValidator = Joi.object({
     title: Joi.string().required().min(3).max(256), // titolo obligatorio
     description: Joi.string().required().min(3),// descrizione obligatorio
     status: Joi.string().valid('public', 'draft','delete','archived').default('draft'),// status di default mette draft
+    eventDate: Joi.date().required(), // mette data creazione post se non inviato
     tag: Joi.array().items(Joi.string().min(3).max(24)), // tag come array di stringhe
     img: Joi.string(),
     imagePost: Joi.string(),
-    locality: Joi.string().uri({ scheme: ['http', 'https'] }).min(3).max(256), // deve essere un URL valido se presente
+    locality: Joi.string().uri({ scheme: ['http', 'https'] }).min(3).max(256).required(), // obbligatorio e deve essere un URL valido
     levelScore: Joi.number().default(0)
 
 }).unknown(true);
